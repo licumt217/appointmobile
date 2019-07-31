55a90081d9c6e8030806283437e539dcbc0bfa9d
liqiang <447818666@qq.com>
2019-07-31 20:55:30 +0800

    add test

--- src/pages/user/login.vue
+++ src/pages/user/login.vue
@@ -90,19 +90,51 @@
 
             let code =Util.getUrlParam("code")
 
-            alert(Util.getUrlParam("code"))
             this.http.get('getOpenId', {
                 params:{
                     code:code
                 }
-            }).then((data) => {
+            }).then((openid) => {
 
-                alert(data)
-                alert(JSON.stringify(data))
+                this.http.post('unifiedOrder', {
+                    openid:openid,
+                    out_trade_no:String(Math.random()).split(".")[1],
+                    total_fee:0.01,
+                }).then((obj) => {
+
+
+
+                    alert(JSON.stringify(obj))
+
+
+                    setTimeout(()=>{
+                        WeixinJSBridge.invoke(
+                            'getBrandWCPayRequest', obj,
+                            function(res){
+                                if(res.err_msg == "get_brand_wcpay_request:ok" ){
+                                    alert("支付成功")
+                                    // 使用以上方式判断前端返回,微信团队郑重提示：
+                                    //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+                                }else{
+                                    alert("支付失败")
+                                }
+                            });
+                    },5000)
+
+
+
+
+                }).catch(err => {
+                    this.$Message.error(err)
+                })
+
+
+
 
             }).catch(err => {
                 this.$Message.error(err)
             })
+
 
             return;
 
