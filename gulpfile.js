f31bcc32ee6819041750b0e04691543385e9c831
root <447818666@qq.com>
2020-05-06 12:16:44 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,15 @@
 let gulp = require('gulp') //加载gulp模块
 let pro = require('child_process') //运行子进程库
 let sftp = require('gulp-sftp') //sftp上传程序
-let username='qiang.li';
+let username='root';
 let password='y2pe2NF7'
 let project_name='appointmobile'
 
 let assets = process.cwd() + `/build`,
     remoteServer1Prep = {
-        host: '123.56.7.118',
+        host: '47.92.74.29',
         port: 22444,
-        remotePath: `/server/nginx/html/${project_name}/${project_name}`,
-        user: username,
-        pass: password
-    },
-    remoteServer2Prep = {
-        host: '123.56.8.169',
-        port: 22444,
-        remotePath: `/server/nginx/html/${project_name}/${project_name}`,
+        remotePath: `/server/nginx/html/${project_name}`,
         user: username,
         pass: password
     }
@@ -24,7 +17,7 @@ let assets = process.cwd() + `/build`,
 
 const build = () => {
     return new Promise((resolve, reject) => {
-        pro.exec('yarn realPrep' , (error, stdout, stderr) => {
+        pro.exec('yarn build' , (error, stdout, stderr) => {
             if (error !== null) {
                 console.log('exec error: ' + error)
             }
@@ -37,5 +30,5 @@ const build = () => {
 //先执行build命令打包。在把程序上传至服务器
 gulp.task('prep', () => build().then(() => {
     console.error(assets)
-    gulp.src(assets + '/**').pipe(sftp(remoteServer1Prep)).pipe(sftp(remoteServer2Prep))
+    gulp.src(assets + '/**').pipe(sftp(remoteServer1Prep))
 }))
