dcfcab4fab52ce037cef91f937af1f3e82062b82
root <447818666@qq.com>
2020-07-08 19:52:22 +0800

    预检表部分添加

---
+++ src/assets/js/constants/QUESTION_TYPE.js
@@ -0,0 +1,15 @@
+/**
+ * 量表问题类型
+ * @type {{NO: number, YES: number}}
+ */
+let QUESTION_TYPE = {
+    Wenda: "0",
+    Zhidaoyu: "1",
+    Xuanzheti: "2",
+    Matrix: "3",
+    Multi: "4",
+    Huang: "5",
+};
+
+
+export default QUESTION_TYPE
