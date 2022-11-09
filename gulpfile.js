9cf7ecbbcfabe97e798ad94675cb5fabc58ce1e0
root <447818666@qq.com>
2022-11-09 16:18:52 +0800

    升级gulp兼容 node 16

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 let gulp = require('gulp') //加载gulp模块
 let pro = require('child_process') //运行子进程库
-let sftp = require('gulp-sftp') //sftp上传程序
-let username='root';
-let password='y2pe2NF7y2pe2NF7'
-let project_name='appointmobile'
+const sftp = require('gulp-sftp-up4'); //sftp上传程序
+let username = 'root';
+let password = 'y2pe2NF7y2pe2NF7'
+let project_name = 'appointmobile'
 
 
 let assets = process.cwd() + `/build`,
     remoteServer1Prep = {
-        host: '47.92.74.29',
+        host: '47.108.78.245',
         port: 22444,
         remotePath: `/server/html/${project_name}`,
         user: username,
@@ -18,7 +18,7 @@ let assets = process.cwd() + `/build`,
 
 const build = () => {
     return new Promise((resolve, reject) => {
-        pro.exec('yarn build' , (error, stdout, stderr) => {
+        pro.exec('yarn build', (error, stdout, stderr) => {
             if (error !== null) {
                 console.log('exec error: ' + error)
             }
